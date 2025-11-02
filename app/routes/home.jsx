bc9336e23a688421e82b8eedb444bdb55446ce1c
KadlecJan <kadlecj22@spstrutnov.cz>
2025-11-02 21:00:22 +0100

    initial commit

---
+++ app/routes/home.jsx
@@ -0,0 +1,12 @@
+export function meta() {
+  return [
+    { title: "FeedBack" },
+    { name: "description", content: "Welcome to FeedBack!" },
+  ];
+}
+
+export default function Home() {
+  return (
+    <div className="bg-[--color-primary] p-4">proměnné tailwind v app.css</div>
+  );
+}
