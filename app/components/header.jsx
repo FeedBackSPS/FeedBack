ea9120237bc040838a91c75b51cce8ebb3286359
PetrasS22 <PetrasS22@spstrutnov.cz>
2025-11-06 13:15:28 +0100

    Header.jsx - doděláno

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -2,12 +2,12 @@ export default function Header() {
     return (
         <header>
             <div className="h-8 bg-lightBlue" ></div>
-            <nav className="bg-deepBlue flex lg:gap-96 md:gap-40 justify-center items-center p-4">
+            <nav className="bg-deepBlue flex lg:gap-96 md:gap-y-64 justify-center items-center p-4">
                 <div>
-                    <img className=""  src="/logo.svg" alt="logo" />
+                    <img className="mr-32"  src="/logo.svg" alt="logo" />
                 </div>
                 <div>
-                    <h1 className="text-sky-100 text-xl ">LOG-OUT</h1>
+                    <h1 className="text-sky-100 text-xl ml-32 ">LOG-OUT</h1>
                 </div>
             </nav>
         </header>
