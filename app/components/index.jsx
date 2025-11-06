cd8f0e6a3119cf6bdd350d707248242c7d57ad7a
Prajzlert22 <prajzlert22@spstrutnov.cz>
2025-11-06 14:02:22 +0100

    Přidání index.jsx

---
+++ app/components/index.jsx
@@ -0,0 +1,19 @@
+import Header from "../components/header.jsx";
+import Footer from "../components/footer.jsx";
+import React from 'react';
+
+
+
+  return (
+    <>
+      <Header />
+
+    <main>
+
+
+        <h1>test</h1>
+    </main>
+
+    <Footer />
+    </>
+  );
